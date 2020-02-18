4b2942512f27702deeafab439459d3b88a0afbcf
Alejandro Picazo <picazo@polygonalmind.com>
2020-02-18 16:17:18 +0100

    MOMUS 2 deploy & optimizations

--- External/MOMUS_02/src/imports/index.ts
+++ External/MOMUS_02/src/imports/index.ts
@@ -1,10 +1,3 @@
 
-export { TagComponent, Elevator, ElevatorButton } from "./components/components"
-export { MoveComponent, MovementType } from "./components/movement"
-export { FollowPathMoveComponent, PathPoint, Path, PathFollower, OnFinishPathBehavior } from "./components/pathFollow"
-export { DoorComponent, DoorTriggerBehaviour } from "./components/doors"
-export { TrapDoorTrigger, trapDoorTriggersInfo } from "./components/triggers"
-export { UpdateMovementComponentsSystem, UpdateToPlayerSystem, UpdateTimerWidgetSystem} from "./systems"
-export { Widget, WidgetTalk, SkipMode, WidgetTextBox, TextProperties, WidgetTextTimmer } from "./widgets"
-export { PlatformComponent } from "./components/platforms"
+export { TagComponent } from "./components/components"
 export { NFTdata } from "./components/NFT"
