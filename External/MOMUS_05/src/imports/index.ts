904c81779022d7544a3d01e5e92e6ad5b456b4b3
Alejandro Picazo <picazo@polygonalmind.com>
2020-02-18 18:15:36 +0100

    MOMUS 5 deploy

--- External/MOMUS_05/src/imports/index.ts
+++ External/MOMUS_05/src/imports/index.ts
@@ -1,5 +1,5 @@
 
-export { TagComponent, Elevator, ElevatorButton } from "./components/components"
+export { TagComponent } from "./components/components"
 export { MoveComponent, MovementType } from "./components/movement"
 export { FollowPathMoveComponent, PathPoint, Path, PathFollower, OnFinishPathBehavior } from "./components/pathFollow"
 export { UpdateMovementComponentsSystem } from "./systems"
